a74162ec51976d2de7a315924b48e63e3acb7db8
Heaven <mc5223@student.uni-lj.si>
2021-01-03 16:40:48 +0100

    Fixed some files.

--- app/src/main/java/com/example/weightmanagmentapp/MainActivity.java
+++ app/src/main/java/com/example/weightmanagmentapp/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.weightmanagmentapp;
 
+import android.annotation.SuppressLint;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue requestQueue;
+    private TextView osebe;
+    private String url = "https://wmanage.azurewebsites.net/api/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
