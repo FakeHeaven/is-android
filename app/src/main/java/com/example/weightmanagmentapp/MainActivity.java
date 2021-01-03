5677b9b442579cb44bc97129f1e0b6ef7a1659c3
Heaven <mc5223@student.uni-lj.si>
2021-01-03 22:43:35 +0100

    Transition after login.

--- app/src/main/java/com/example/weightmanagmentapp/MainActivity.java
+++ app/src/main/java/com/example/weightmanagmentapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.weightmanagmentapp;
 
+import android.content.Intent;
 import android.util.Log;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,21 +10,25 @@ import android.view.View;
 import android.widget.TextView;
 import com.android.volley.*;
 import com.android.volley.toolbox.*;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
+import java.security.PrivateKey;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String id;
     private RequestQueue requestQueue;
-    private TextView token;
     private EditText email;
+    public static int test;
     private EditText password;
-
+    private static String token;
     private String url = "https://wmanage.azurewebsites.net/api/token";
     private TextView status;
 
@@ -33,11 +38,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         email = (EditText) findViewById(R.id.teEmail);
         password = (EditText) findViewById(R.id.tePassword);
-        token = (TextView) findViewById(R.id.submit);
+        status = (TextView) findViewById(R.id.status);
         requestQueue = Volley.newRequestQueue(getApplicationContext());
     }
     public void getToken(View view){
-
+        final String EXTRA_MESSAGE = "com.example.universityapp.MESSAGE";
         try {
             RequestQueue requestQueue = Volley.newRequestQueue(this);
             JSONObject jsonBody = new JSONObject();
@@ -48,14 +53,26 @@ public class MainActivity extends AppCompatActivity {
             StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
                 @Override
                 public void onResponse(String response) {
+
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                    MainActivity.id = jsonObject.getString("userId");
+                    MainActivity.token = jsonObject.getString("accessToken");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                     Log.i("LOG_RESPONSE", response);
+
+//                    status.setText(response);
                 }
+
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
                     Log.e("LOG_RESPONSE", error.toString());
                 }
             }) {
+
                 @Override
                 public String getBodyContentType() {
                     return "application/json; charset=utf-8";
@@ -71,22 +88,29 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
 
-//                @Override
-//                protected Response<String> parseNetworkResponse(NetworkResponse response) {
-//                    String responseString = "";
-//                    if (response != null) {
-//                        responseString = String.valueOf(response.statusCode);
-//                    }
-//                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
-//                }
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    if (response != null) {
+                        responseString = String.valueOf(response.statusCode);
+                        status.setText(responseString);
+                        test = Integer.parseInt(responseString);
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                }
+
             };
 
             requestQueue.add(stringRequest);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
+        if (test == 200){
+            Intent intent = new Intent(this,MainPage.class);
+            String message = "Uspesno si se prijavil.";
+            intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
+        }
     }
-
 
 }
