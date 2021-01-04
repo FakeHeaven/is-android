5b0db8c6665ccec7ca895f22fd470b8a45fd8f7a
Heaven <mc5223@student.uni-lj.si>
2021-01-04 20:01:17 +0100

    User Create done.

--- app/src/main/java/com/example/weightmanagmentapp/Subuser.java
+++ app/src/main/java/com/example/weightmanagmentapp/Subuser.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +18,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -23,6 +26,8 @@ import java.util.Map;
 public class Subuser extends AppCompatActivity {
     String id;
     String token;
+    private EditText Name;
+    private EditText Date;
     private TextView teDisplay;
     private String url = "https://wmanage.azurewebsites.net/api/subusers";
 
@@ -56,6 +61,8 @@ public class Subuser extends AppCompatActivity {
                 return false;
             }
         });
+        Name = (EditText) findViewById(R.id.teName);
+        Date = (EditText) findViewById(R.id.teDate);
         teDisplay();
 
     }
@@ -72,7 +79,7 @@ public void teDisplay(){
             try {
                 jsonObject= new JSONArray(response);
                 ArrayList<String> data = new ArrayList<>();
-                for (int i = 0; i < jsonObject.length(); i++){
+                for (int i = 1; i < jsonObject.length(); i++){
                     try {
                         JSONObject object =jsonObject.getJSONObject(i);
                         String uid = object.getString("id");
@@ -136,4 +143,70 @@ public void teDisplay(){
     };
         requestQueue.add(stringRequest);
 }
+
+    public void subCreate(View view) {
+        try {
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("userId", id);
+            jsonBody.put("name", Name.getText());
+            jsonBody.put("dateOfBirth", Date.getText());
+            final String mRequestBody = jsonBody.toString();
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+//                    System.out.println(response);
+                    Log.i("LOG_RESPONSE", response);
+//                    status.setText(response);
+                }
+
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("LOG_RESPONSE", error.toString());
+                }
+            }) {
+
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", mRequestBody, "utf-8");
+                        return null;
+                    }
+                }
+
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    HashMap<String, String> headers = new HashMap<String, String>();
+                    headers.put("Content-Type", "application/json");
+                    headers.put("Authorization", "Bearer " + token);
+                    return headers;
+                }
+
+//                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+//                    String responseString = "";
+//                    if (response != null) {
+//                        responseString = String.valueOf(response.statusCode);
+//                        status.setText(responseString);
+//                        test = Integer.parseInt(responseString);
+//                    }
+//                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+//                }
+
+            };
+
+            requestQueue.add(stringRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        teDisplay();
+    }
 }
